4533135dae9d6d8acbd3abf00a788774bac43904
sachithmuhandiram <sachithnalaka@gmail.com>
2021-05-09 20:33:52 +0530

    fix: fixed initial point to 1,1

--- movement.hpp
+++ movement.hpp
@@ -1,32 +1,32 @@
 #include <ncurses.h>
 #include <stdio.h>
 
-void moveUp();
-void moveDown();
-void moveLeft();
-void moveRight(); 
+void moveUp(WINDOW *);
+void moveDown(WINDOW *);
+void moveLeft(WINDOW *);
+void moveRight(WINDOW *); 
 void display();
 
 int yMax =15;
 int xMax =15;
 int yLocation,xLocation;
+    
+int height=15;
+int width=15;
+int start_x=1;
+int start_y=1;
 
 void display(){
 
     initscr(); // this must be top
-    int height,width,start_x,start_y;
-    
-    height=15;
-    width=15;
-    start_x=start_y=1;
-    xLocation=0;
+    xLocation=1;
     yLocation=1;
-
-    WINDOW *win = newwin(height,width,start_y,start_x); 
     
+    WINDOW *win = newwin(height,width,start_y,start_x); 
     box(win,0,0);
 
     refresh();
+    mvwaddch(win,1,1,'f');
     wrefresh(win);
     keypad(win,true);
     // above two should be in line
@@ -40,60 +40,67 @@ void display(){
         switch (choices)
         {
         case KEY_UP:
-            moveUp();
+            
+            moveUp(win);
             break;
         case KEY_DOWN:
-            moveDown();
+            
+            moveDown(win);
             break;
         case KEY_LEFT:
-            moveLeft();
+            
+            moveLeft(win);
             break;
 
         case KEY_RIGHT:
-            moveRight();
+            
+            moveRight(win);
             break;
         default:
             break;
         }
-        wrefresh(win);
         mvwaddch(win,yLocation,xLocation,'f');
+        wrefresh(win);
         
     }
     endwin();
 }
 
-void moveUp(){
+void moveUp(WINDOW *w){
     yLocation--;
 
     if (yLocation <1)
     {
-        //std::cout << "up" << std::endl;
         yLocation =1;
     }
+    mvwaddch(w,yLocation+1,xLocation,' ');
 }
 
-void moveDown(){
+void moveDown(WINDOW *win){
     yLocation++;
 
     if (yLocation > yMax-2)
     {
         yLocation = yMax-2; 
     }
+    mvwaddch(win,yLocation-1,xLocation,' ');
 }
 
-void moveLeft(){
+void moveLeft(WINDOW *win){
     xLocation--;
 
     if (xLocation < 1)
     {
         xLocation = 1;
     }
+        mvwaddch(win,yLocation,xLocation+1,' ');
 }
 
-void moveRight(){
+void moveRight(WINDOW *win){
     xLocation++;
     if (xLocation > xMax-2)
     {
         xLocation = xMax-2;
     }
+    mvwaddch(win,yLocation,xLocation-1,' ');
 }
